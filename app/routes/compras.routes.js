0e2a71a5eeb22ee2b530f92cb7ce7be5d92f4f8b
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-05 15:13:04 -0500

    Fix startup error in compras routes

--- app/routes/compras.routes.js
+++ app/routes/compras.routes.js
@@ -18,23 +18,5 @@ router.get('/compras-dos-consultas-test', requireSession, comprasDosConsultasCon
 router.get('/compras-ping-test', requireSession, comprasPingController.comprasPingTest);
 router.get('/compras-lotes-test', requireSession, comprasLotesController.comprasLotesTest);
 router.get('/compras-sin-order-test', requireSession, comprasSinOrderController.comprasSinOrderTest);
-router.get('/proveedores-test', requireSession, controller.proveedoresTest);
-router.get('/conexion-test', requireSession, controller.conexionTest);
-router.get('/proveedores-ruc-test', requireSession, controller.proveedoresRucTest);
-router.get('/proveedores-ruc-sin-order-test', requireSession, controller.proveedoresRucSinOrderTest);
-router.get('/proveedores-ruc-cast-test', requireSession, controller.proveedoresRucCastTest);
-router.get('/proveedores-ruc-fijo-test', requireSession, controller.proveedoresRucFijoTest);
-router.get('/proveedores-nomprovee-test', requireSession, controller.proveedoresNomproveeTest);
-router.get('/proveedores-nomprovee-cast-test', requireSession, controller.proveedoresNomproveeCastTest);
-router.get('/proveedores-nomprovee-dedicado-test', requireSession, controller.proveedoresNomproveeDedicadoTest);
-router.get('/proveedores-join-dedicado-test', requireSession, controller.proveedoresJoinDedicadoTest);
-router.get('/proveedores-join-nom-length-dedicado-test', requireSession, controller.proveedoresJoinNomLengthDedicadoTest);
-router.get('/proveedores-join-concat-dedicado-test', requireSession, controller.proveedoresJoinConcatDedicadoTest);
-router.get('/proveedores-join-varchar-dedicado-test', requireSession, controller.proveedoresJoinVarcharDedicadoTest);
-router.get('/proveedores-join-array-dedicado-test', requireSession, controller.proveedoresJoinArrayDedicadoTest);
-router.get('/proveedores-join-bytes-dedicado-test', requireSession, controller.proveedoresJoinBytesDedicadoTest);
-router.get('/proveedores-ruc-lista-dedicado-test', requireSession, controller.proveedoresJoinArrayDedicadoTest2);
-router.get('/pool-status-test', requireSession, controller.poolStatusTest);
-router.get('/pool-select-test', requireSession, controller.poolSelectTest);
 
 module.exports = router;
