6934d69bbd642e8b8db19ce03d6c97c3df8a78ec
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-05 14:34:19 -0500

    Agregar endpoint mínimo para probar respuesta HTTP

--- app/routes/compras.routes.js
+++ app/routes/compras.routes.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/compras.controller');
 const comprasDiccionarioController = require('../controllers/compras-diccionario-test');
 const comprasJsonController = require('../controllers/compras-json-test');
 const comprasDosConsultasController = require('../controllers/compras-dos-consultas-test');
+const comprasPingController = require('../controllers/compras-ping-test');
 const { requireSession } = require('../auth/session');
 
 const router = express.Router();
@@ -12,6 +13,7 @@ router.get('/compras-diccionario-test', requireSession, comprasDiccionarioContro
 router.get('/compras-conexion-test', requireSession, comprasDiccionarioController.comprasConexionTest);
 router.get('/compras-json-test', requireSession, comprasJsonController.comprasJsonTest);
 router.get('/compras-dos-consultas-test', requireSession, comprasDosConsultasController.comprasDosConsultasTest);
+router.get('/compras-ping-test', requireSession, comprasPingController.comprasPingTest);
 router.get('/proveedores-test', requireSession, controller.proveedoresTest);
 router.get('/conexion-test', requireSession, controller.conexionTest);
 router.get('/proveedores-ruc-test', requireSession, controller.proveedoresRucTest);
