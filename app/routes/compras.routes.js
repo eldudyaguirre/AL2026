a2c3a5ad5e7e205e8ff0f9754645d52612156be9
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-05 07:44:09 -0500

    Agregar diagnóstico de RUC de proveedores

--- app/routes/compras.routes.js
+++ app/routes/compras.routes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/compras', requireSession, controller.compras);
 router.get('/proveedores-test', requireSession, controller.proveedoresTest);
 router.get('/conexion-test', requireSession, controller.conexionTest);
+router.get('/proveedores-ruc-test', requireSession, controller.proveedoresRucTest);
 
 module.exports = router;
