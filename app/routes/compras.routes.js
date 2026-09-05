59442d51785da1968f59bd11964f7618ad891f5a
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-05 09:51:03 -0500

    Agregar endpoint diagnóstico rowMode array

--- app/routes/compras.routes.js
+++ app/routes/compras.routes.js
@@ -18,6 +18,7 @@ router.get('/proveedores-join-dedicado-test', requireSession, controller.proveed
 router.get('/proveedores-join-nom-length-dedicado-test', requireSession, controller.proveedoresJoinNomLengthDedicadoTest);
 router.get('/proveedores-join-concat-dedicado-test', requireSession, controller.proveedoresJoinConcatDedicadoTest);
 router.get('/proveedores-join-varchar-dedicado-test', requireSession, controller.proveedoresJoinVarcharDedicadoTest);
+router.get('/proveedores-join-array-dedicado-test', requireSession, controller.proveedoresJoinArrayDedicadoTest);
 router.get('/pool-status-test', requireSession, controller.poolStatusTest);
 router.get('/pool-select-test', requireSession, controller.poolSelectTest);
 
