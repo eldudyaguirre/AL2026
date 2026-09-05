0e058669ce3cec9029b727e17f10e4d5b83d5699
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-05 08:04:35 -0500

    Agregar ruta diagnostica ORDER BY con LIMIT fijo

--- app/routes/compras.routes.js
+++ app/routes/compras.routes.js
@@ -10,6 +10,7 @@ router.get('/conexion-test', requireSession, controller.conexionTest);
 router.get('/proveedores-ruc-test', requireSession, controller.proveedoresRucTest);
 router.get('/proveedores-ruc-sin-order-test', requireSession, controller.proveedoresRucSinOrderTest);
 router.get('/proveedores-ruc-cast-test', requireSession, controller.proveedoresRucCastTest);
+router.get('/proveedores-ruc-fijo-test', requireSession, controller.proveedoresRucFijoTest);
 router.get('/pool-status-test', requireSession, controller.poolStatusTest);
 router.get('/pool-select-test', requireSession, controller.poolSelectTest);
 
