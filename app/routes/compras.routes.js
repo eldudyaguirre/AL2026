cda0bb0841e5b5f23365ff7b1e2b875bb65a9aa0
eldudyaguirre <143123379+eldudyaguirre@users.noreply.github.com>
2026-09-08 22:40:33 -0500

    Agregar ruta para reporte detallado por areas

--- app/routes/compras.routes.js
+++ app/routes/compras.routes.js
@@ -14,6 +14,7 @@ const router = express.Router();
 
 router.get('/compras', requireSession, controller.compras);
 router.get('/compras/proyectos', requireSession, proyectosController.resumenGastosPorProyecto);
+router.get('/compras/reporte-area', requireSession, proyectosController.reporteDetallePorArea);
 router.get('/compras-solo-test', requireSession, comprasSoloController.comprasSoloTest);
 router.get('/compras-diccionario-test', requireSession, comprasDiccionarioController.comprasDiccionarioTest);
 router.get('/compras-conexion-test', requireSession, comprasDiccionarioController.comprasConexionTest);
